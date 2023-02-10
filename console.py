828d443e7deb7a7553102ce0b095845626121ed5
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-02-10 15:58:26 +0300

    AirBnB Clone

--- console.py
+++ console.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 """ This module implements a command interpreter for AirBnB clone"""
 import cmd
+import os
 from models.__init__ import storage
 
 class HBNBCommand(cmd.Cmd):
     """AirBnB command interpreter class definition"""
     prompt = '(hbnb) '
 
-    """def __init__(self):
-        Object constructor method
-        super.__init__()"""
+    def __init__(self, completekey='tab', stdin=None, stdout=None):
+        """Object constructor method"""
+        super().__init__(completekey, stdin, stdout)
 
     def do_update(self, args):
         """Updates instance specified by Class and id
@@ -47,7 +48,9 @@ class HBNBCommand(cmd.Cmd):
         storage.save()
 
     def do_all(self, args):
-        """Prints list of string representation of all stored objects"""
+        """Prints list of string representation of stored objects
+        USAGE: all [ClassName] - for all instances of specified class
+        USAGE: all - for all instances of all classes"""
         if len(args) > 0 and storage.my_classes(args) is None:
             return
         obj_strs = []
@@ -95,20 +98,27 @@ class HBNBCommand(cmd.Cmd):
             print(obj)
 
     def do_create(self, args):
-        """Creates a new instance of a class"""
+        """Creates a new instance of a class
+        USAGE: create [ClassName]"""
         class_name = storage.my_classes(args)
         if class_name is not None:
             new_class = class_name()
             new_class.save()
             print(new_class.id)
 
+    def do_shell(self, args):
+        """Provides shell functionalities within interpreter
+        USAGE: shell [shell commands & arguments]
+        EXMPL: shell ls -l"""
+        os.system(args)
+
     def do_EOF(self, args):
-        """This method handles the EOF condition"""
+        """Handles the EOF condition"""
         print()
         return True
 
     def do_quit(self, args):
-        """This method ends the program when the command 'quit' is received"""
+        """Ends the program when the command 'quit' is received"""
         return True
 
     def emptyline(self):
