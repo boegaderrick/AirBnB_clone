c673ce1bfcf9dc28c7b492a81249c3d8e761cdbc
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-02-12 13:43:44 +0300

    AirBnB Clone

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -3,10 +3,21 @@
 
 import unittest
 from models.base_model import BaseModel
+import datetime
+import time
 
 
 class TestBaseClass(unittest.TestCase):
     """BaseModel tests class"""
     def test_1(self):
+        """Performs tests on BaseModel class"""
         n = BaseModel()
+        delta = (n.updated_at - n.created_at).total_seconds()
+        v = all(i in n.__str__() for i in ['id', 'created_at', 'updated_at'])
+        self.assertTrue(v)
         self.assertEqual(len(n.id), 36)
+        self.assertAlmostEqual(delta, 0, places=4)
+        time.sleep(1.1)
+        n.save()
+        delta = (n.updated_at - n.created_at).total_seconds()
+        self.assertGreater(delta, 1)
