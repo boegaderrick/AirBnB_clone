560447552a612e57031f290e4dbd866e69a144a1
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-02-12 19:24:03 +0300

    AirBnB Clone

---
+++ tests/test_console.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""This module contains a test class for AirBnB console testing"""
+
+from unittest import TestCase
+from console import HBNBCommand
+from unittest.mock import patch
+from io import StringIO
+
+
+class TestHBNBCommand(TestCase):
+    """AirBnB console test class"""
+    def test_1(self):
+        """Tests output of the help command"""
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("help show")
+            string = " Prints string representation of instance specified\n\
+        USAGE: show [ClassName] [id]\n"
+            output = f.getvalue()
+            self.assertEqual(string, output)
+
+    def test_2(self):
+        """Tests output of 'no command + enter' or emptyline"""
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("")
+            string = ''
+            output = f.getvalue()
+            self.assertEqual(string, output)
