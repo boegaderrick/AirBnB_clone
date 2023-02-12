fe2923c84c8f92db393d5f411db0c224c3cec4c4
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-02-13 00:33:17 +0300

    AirBnB Clone

---
+++ tests/test_models/test_state.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This module contains tests for the State class of AirBnB"""
+
+from models.state import State
+from unittest import TestCase
+
+
+class TestState(TestCase):
+    """This class defines test methods for the State class"""
+    def test_1(self):
+        """Tests State class attributes"""
+        s = State()
+        self.assertTrue(len(s.name) ==  0)
+        self.assertTrue(type(s.name) is str)
