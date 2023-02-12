6e8bbcde5c5a92bfbf57fdd88fc03891ced73cca
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-02-12 19:48:03 +0300

    AirBnB Clone

--- tests/test_console.py
+++ tests/test_console.py
@@ -9,7 +9,7 @@ from io import StringIO
 
 class TestHBNBCommand(TestCase):
     """AirBnB console test class"""
-    def test_1(self):
+    def test_help_show(self):
         """Tests output of the help command"""
         with patch('sys.stdout', new=StringIO()) as f:
             HBNBCommand().onecmd("help show")
@@ -18,7 +18,19 @@ class TestHBNBCommand(TestCase):
             output = f.getvalue()
             self.assertEqual(string, output)
 
-    def test_2(self):
+    def test_create(self):
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("create")
+            string = '** class name missing **\n'
+            self.assertEqual(string, f.getvalue())
+            print(f.getvalue())
+
+        #with patch('sys.stdout', new=StringIO()) as f:
+        #    HBNBCommand().onecmd("create BaseModel")
+        #    self.assertEqual(36, len(f.getvalue()))
+
+
+    def test_no_command(self):
         """Tests output of 'no command + enter' or emptyline"""
         with patch('sys.stdout', new=StringIO()) as f:
             HBNBCommand().onecmd("")
@@ -26,7 +38,7 @@ class TestHBNBCommand(TestCase):
             output = f.getvalue()
             self.assertEqual(string, output)
 
-    def test_3(self):
+    def test_quit_EOF(self):
         """Tests the quit and EOF commands"""
         with patch('sys.stdout', new=StringIO()) as f:
             HBNBCommand().onecmd("quit")
