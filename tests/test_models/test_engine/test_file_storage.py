6350d42391c1d8920b0102b5d4db5d5135ed4e43
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-02-12 15:34:49 +0300

    AirBnB Clone

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This module contains the FileStorage class unit tests"""
+
+from unittest import TestCase
+from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
+
+
+class TestFileStorage(TestCase):
+    """FileStorage class test class definition"""
+    def test_1(self):
+        """Performs tests on the FileStorage class"""
+        b = BaseModel()
+        f = FileStorage()
+        self.assertTrue(b in f.all().values())
